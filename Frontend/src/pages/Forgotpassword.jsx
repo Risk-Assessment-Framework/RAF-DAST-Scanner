787876bfc91511b7f721e0119d036d8cac0bbac0
shubhayu-64 <majumdarshubhayu@gmail.com>
2023-02-19 20:15:17 +0530

    Added Forgot Password functions

--- Frontend/src/pages/Forgotpassword.jsx
+++ Frontend/src/pages/Forgotpassword.jsx
@@ -3,25 +3,24 @@ import { useUserAuth } from "../context/UserAuthContext";
 import { useNavigate } from "react-router-dom";
 import { XCircleIcon } from "@heroicons/react/20/solid";
 
-const Login = () => {
+const Forgotpassword = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const { signIn, signInWithGoogle } = useUserAuth();
+  const { forgotPassword, signInWithGoogle } = useUserAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
     try {
-      await signIn(email, password);
+      await forgotPassword(email, password);
       navigate("/");
     } catch (err) {
       setError(err.message);
       console.log(err);
     }
   };
-
   const handleGoogleSignIn = async (e) => {
     e.preventDefault();
     setError("");
@@ -46,7 +45,7 @@ const Login = () => {
             />
           </a>
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
+            Recover your account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{" "}
@@ -54,7 +53,7 @@ const Login = () => {
               href="/signup"
               className="font-medium text-indigo-600 hover:text-indigo-500"
             >
-              new to us? Sign up
+              Sign in
             </a>
           </p>
         </div>
@@ -84,27 +83,6 @@ const Login = () => {
                 </div>
               </div>
 
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                    required
-                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
               {error && (
                 <div className="rounded-md bg-red-50 p-4">
                   <div className="flex">
@@ -123,38 +101,12 @@ const Login = () => {
                 </div>
               )}
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="/forgotpassword"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-
               <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                  Sign in
+                  Recover
                 </button>
               </div>
             </form>
@@ -215,4 +167,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Forgotpassword;
